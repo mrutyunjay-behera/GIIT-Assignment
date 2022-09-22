bca73e9764055aae8d13fa9c0b1ed9de810a84c1
Mrutyunjay Behera <beherambabu@gmail.com>
2022-09-22 21:24:24 +0530

    Done

--- marketing/src/main/java/com/assessment/repositories/LeadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assessment.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assessment.entities.Lead;
-
-public interface LeadRepository extends JpaRepository<Lead, Long> {
-
-}
